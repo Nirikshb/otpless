04fec6fa2a18fd61e52a5239d1cb374e3d82422f
Niriksh Bharti <111577030+Nirikshb@users.noreply.github.com>
2023-11-30 23:05:33 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import './App.css'; // Import your App.css file
 
-function App() {
+// Custom hook for loading OTPless SDK
+const useLoadOTPlessSDK = () => {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://otpless.com/auth.js";
+    script.async = true;
+
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+};
+
+const OTPlessSignIn = () => {
+  const [userData, setUserData] = useState(null);
+  const [showSignIn, setShowSignIn] = useState(true);
+
+  useLoadOTPlessSDK(); // Load OTPLESS SDK on component mount
+
+  const handleUserData = (otplessUser) => {
+    console.log("User data:", otplessUser);
+    setUserData(otplessUser);
+    setShowSignIn(false); // Hide sign-in UI when user data is available
+  };
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      window.otpless = handleUserData;
+    }
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="container">
+    {showSignIn && (
+      <div id="otpless-login-page" className="signin-container">
+      </div>
+    )}
+    {!showSignIn && userData !== null && (
+      <div className="user-details">
+        <h2>User Details</h2>
+        {userData.mobile && userData.mobile.number && (
+          <div className="user-data">
+            <p>Name: {userData.email.name}</p>
+            <p>Number: {userData.mobile.number}</p>
+          </div>
+        )}
+        {userData.email && userData.email.email && (
+          <p>Email: {userData.email.email}</p>
+        )}
+      </div>
+    )}
+  </div>
   );
-}
+};
 
-export default App;
+export default OTPlessSignIn;
